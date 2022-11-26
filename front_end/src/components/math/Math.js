3a83ba6d12e3ae06272ff5093befaa20496a5c8a
cancan007 <shota.moue0211@gmail.com>
2022-11-26 15:15:45 +0900

    try to deploy on heroku

--- front_end/src/components/math/Math.js
+++ front_end/src/components/math/Math.js
@@ -41,6 +41,7 @@ export default function Math() {
   const { data: referencesByTag, refetch: refetchReferencesByTag } =
     useAPIFetchWithTags(formInput, tag, {
       onSuccess: (res) => {
+        if (res.length === 0) return;
         setRefes(res);
       },
     });
